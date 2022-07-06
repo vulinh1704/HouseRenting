7fbf6a5c07c72f71fab457081cd0a8952f1106ff
hoangtranduyLONG <duylonghindu@gmail.com~>
2022-07-06 15:55:06 +0700

    model

--- src/main/java/com/example/houserenting/controller/HouseController.java
+++ src/main/java/com/example/houserenting/controller/HouseController.java
@@ -1,7 +1,66 @@
 package com.example.houserenting.controller;
 
+import com.example.houserenting.model.House;
+import com.example.houserenting.service.impl.HouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
-@Controller
+@RestController
+@RequestMapping("/house")
 public class HouseController {
+    @Autowired
+    private HouseService houseService;
+
+    @GetMapping
+    public ResponseEntity<Page<House>> findAllHouse(@PageableDefault(value = 10) Pageable pageable) {
+        Page<House> houses = houseService.findAll(pageable);
+        if (houses.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(houses, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<House> findById(@PathVariable Long id) {
+        Optional<House> houseOptional = houseService.findById(id);
+        if (!houseOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(houseOptional.get(), HttpStatus.OK);
+    }
+
+//    Cái này là hàm save thường
+    @PostMapping
+    public ResponseEntity<House> saveProduct ( @RequestBody House house) {
+        houseService.save(house);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<House> updateHouse (@PathVariable Long id,@RequestBody House house) {
+        Optional<House> houseOptional = houseService.findById(id);
+        if (!houseOptional.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        house.setId(houseOptional.get().getId());
+        houseService.save(house);
+        return new ResponseEntity<>( HttpStatus.OK);
+    }
+
+
+
+
+
+
+
+
+
 }
