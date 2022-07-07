4fafc75b23e3abac21211a4c55f7c2fdc67465f3
vulinh1704 <truongdangvulinh1704@gmail.com>
2022-07-08 05:51:45 +0700

    cập nhật phân trang

--- src/main/resources/templates/javascript/house.js
+++ src/main/resources/templates/javascript/house.js
@@ -1,5 +1,5 @@
 function htmlHouse() {
-    showHouses()
+    showHouses(0)
     document.getElementById("main").innerHTML =
         `
         <div class="container-fluid p-0">
@@ -19,9 +19,9 @@ function htmlHouse() {
             <div class="row" id="houses">
                 
             </div>
-            <div class="row mt-4">
+            <div class="row mt-5">
                 <div class="col-4"></div>
-                <div class="col-4" id="Pagination"></div>
+                <div class="col-4" id="Pagination" style="background: #4caf8c"></div>
                 <div class="col-4"></div>
             </div>
         </div>
@@ -29,24 +29,25 @@ function htmlHouse() {
         `
 }
 
-function showHouses() {
+function showHouses(number) {
     $.ajax({
         type: "GET",
-        url: 'http://localhost:8080/house',
+        url: 'http://localhost:8080/house?page=' + number,
         success: function (houses) {
             console.log(houses)
             htmlCard(houses.content)
-        }, error : function (error) {
+            pagination(houses)
+        }, error: function (error) {
             console.log(error)
         }
     })
 }
+
 function htmlCard(house) {
     let str = ""
     for (let i = 0; i < house.length; i++) {
         let idCa = "carousel" + house[i].id
-        let a = "#" + idCa
-        console.log(idCa)
+        let a = "#" + idCa;
         str += `
                 <div class="col-4 mt-4">
                     <div class="card" style="width: 18rem;">
@@ -76,21 +77,63 @@ function htmlCard(house) {
                         <p>${house[i].address}</p>
                         <p>${house[i].price}</p>
                         <hr>
-                        <button>Xem chi tiết</button>
+                        <button onclick="seeDetails(${house[i].id})">Xem chi tiết</button>
                       </div>
                     </div>
                 </div>`
     }
-    // let page = "";
-    // let num = house.pageable.pageNumber
-    // console.log(num)
-    // if(num > 0 || num+1 === house.totalPages) {
-    //     page += `<button onclick="showHouses(${num-1})">Trước</button>`
-    // }
-    // page += `${num+1}/${house.totalPages}`
-    // if(num <= 0 || num+1 !== house.totalPages) {
-    //     page += `<button onclick="showHouses(${num+1})">Sau</button>`
-    // }
-    // document.getElementById("Pagination").innerHTML = page;
+
     document.getElementById("houses").innerHTML = str;
+}
+
+function pagination(houses) {
+    let page = ``;
+    let num = houses.pageable.pageNumber;
+    console.log(num)
+    if (num > 0 || num + 1 === houses.totalPages) {
+        page += `<span onclick="showHouses(${num - 1})" style="color: #FFFFFF"><i class="fa-solid fa-circle-chevron-left"></i> Trước| </span>`
+    }
+    page += `<span style="color: white">${num + 1}/${houses.totalPages}</span>`
+    if (num <= 0 || num + 1 !== houses.totalPages) {
+        page += `<span onclick="showHouses(${num + 1})" style="color: #FFFFFF">  |<i class="fa-solid fa-circle-chevron-right"></i> Sau </span>`
+    }
+
+    document.getElementById("Pagination").innerHTML = page;
+}
+
+function seeDetails(id) {
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/house/" + id,
+        success: function (house) {
+            console.log(house)
+            htmlSeeDetail(house)
+        }, error: function (error) {
+            console.log(error)
+        }
+    })
+}
+function htmlSeeDetail(house) {
+    document.getElementById("main").innerHTML =
+        `
+        <div class="container-fluid p-0">
+            <div class="row" style="background: #4caf8c;width: 100%">
+                <h1 style="color: #FFFFFF;margin-left: 35%;"><img src="https://firebasestorage.googleapis.com/v0/b/comhut-6cb9a.appspot.com/o/output-onlinegiftools.gif?alt=media&token=6cdc0287-cd96-411d-bc4e-be950e86fa8e" style="width: 60px;height: 60px">
+                XEM CHI TIẾT
+                <img src="https://firebasestorage.googleapis.com/v0/b/comhut-6cb9a.appspot.com/o/output-onlinegiftools.gif?alt=media&token=6cdc0287-cd96-411d-bc4e-be950e86fa8e" style="width: 60px;height: 60px"></h1>
+            </div>
+            <div class="row" style="background: #4caf8c;width: 100%">
+                <h1 style="color: #FFFFFF;margin-left: 20%;">CHÀO MỪNG BẠN ĐẾN ${house.name} !!</h1>
+            </div>
+            <div class="row" id="houses">
+                
+            </div>
+            <div class="row mt-5">
+                <div class="col-4"></div>
+                <div class="col-4" id="Pagination" style="background: #4caf8c"></div>
+                <div class="col-4"></div>
+            </div>
+        </div>
+        
+        `
 }
